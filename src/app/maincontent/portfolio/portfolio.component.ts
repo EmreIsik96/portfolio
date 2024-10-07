4aced1e609c16586983321fa7cd078f22ffa7b12
Emre Isik <emre-isik@web.de>
2024-10-07 14:27:10 +0200

    add portfolio component and give style with SCSS

--- src/app/maincontent/portfolio/portfolio.component.ts
+++ src/app/maincontent/portfolio/portfolio.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+ 
+  projects = [{
+    images: "./assets/projectsOnLaptop/join.png",
+    name:  "Join",
+    language: "Angular | TypeScript | HTML | CSS | Firebase",
+    description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories."
+  },
+  {
+    images: "./assets/projectsOnLaptop/sharkie.png",
+    name: "El Pollo Loco",
+    language: "JavaScript | HTML | CSS",
+    description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories."
+  },
+  {
+    images: "./assets/projectsOnLaptop/pokedex.png",
+    name: "Pokèdex",
+    language: "JavaScript | HTML | CSS | Api",
+    description: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.."
+  }]
 }
